176976ed37447d3e2aa5646e0065e08d3bc7f6b7
Jannis <rex.jannis@web.de>
2019-03-31 19:49:51 +0200

    needs to be cleaner xD
    Signed-off-by: Jannis <rex.jannis@web.de>

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class A:
-    def CalculatePersistence(self, x, iterations=0):
+    def CalculatePersistence(self, x, x_, iterations=0):
         if len(str(x)) == 1:
             print(x)
-            print('Count of Iterations: ', str(iterations))
+            print('\nThe Number \'', str(x_),
+                  '\' has a Multiplicative Persistence of: ', str(iterations))
             return "No more Multiplications possible"
 
         items = [int(i) for i in str(x)]
@@ -17,9 +18,9 @@ class A:
 
         iterations += 1
 
-        self.CalculatePersistence(product, iterations)
+        self.CalculatePersistence(product, x_, iterations)
 
 
 abc = A()
-# abc.CalculatePersistence(77)
-abc.CalculatePersistence(679)
+# abc.CalculatePersistence(77, 77)
+abc.CalculatePersistence(679, 679)
